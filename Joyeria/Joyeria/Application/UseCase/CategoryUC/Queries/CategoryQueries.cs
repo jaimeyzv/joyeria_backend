438295cfa6d2faffe8341a94635ed1214d785e88
Jaime Zamora <jaime.zamora@blankfactor.com>
2023-01-30 15:06:40 -0500

    commit

---
+++ Joyeria/Joyeria/Application/UseCase/CategoryUC/Queries/CategoryQueries.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Joyeria.Application.Interfaces;
+using Joyeria.Application.UseCase.CategoryUC.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joyeria.Application.UseCase.CategoryUC.Queries
+{
+    public class CategoryQueries : ICategoryQueries
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public CategoryQueries(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<CategoryModel>> GetCategoriesAsync()
+        {
+            var models = _unitOfWork.Categories.GetCategoriesAsync();
+            
+        }
+
+        public Task<CategoryModel> GetCategoryByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Joyeria/Joyeria/Application/UseCase/CategoryUC/Queries/ICategoryQueries.cs
@@ -0,0 +1,10 @@
+﻿using Joyeria.Application.UseCase.CategoryUC.Commands;
+
+namespace Joyeria.Application.UseCase.CategoryUC.Queries
+{
+    public interface ICategoryQueries
+    {
+        Task<IEnumerable<CategoryModel>> GetCategoriesAsync();
+        Task<CategoryModel> GetCategoryByIdAsync(int id);
+    }
+}
